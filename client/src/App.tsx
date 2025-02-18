98584eab71106df565a3563f9aaa06bc0a03f948
grigdon <gabrielrigdon@gmail.com>
2025-02-18 11:46:00 -0500

    added new pages and navigation bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { CarProfile  } from './pages/CarProfile'
+import { UserProfile } from './pages/UserProfile'
+import { Layout } from './components/Layout'
 
 function App() {
   return (
    <Router>
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/carprofile" element={<CarProfile/>}/>
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/carprofile" element={<CarProfile/>}/>
+        <Route path="/userprofile" element={<UserProfile/>}/>
+      </Route>
     </Routes>
    </Router>
   )
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,7 @@
-import { Link } from "react-router-dom"
-
 export function Home() {
     return (
         <>
             <h1>This is the home page</h1>
-            <Link to="/">Home</Link>
-            <Link to="/carprofile">Car Profile</Link>
         </>
     )
 }
