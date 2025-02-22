36e8fbd3ae765b4fb15c11b740a634d34088b07b
grigdon <gabrielrigdon@gmail.com>
2025-02-22 11:54:38 -0500

    updated theming and added modal componenet

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom"
+import "./Navbar.css"
+import car from "../../src/assets/car-image.png"
 
 // Static navigation bar with routing to other pages, cojoined with 'Layout'
 
 export function Navbar() {
     return (
-        <>
-            <Link to="/">
-                <button>Home</button>
-            </Link>
-            <Link to="/carprofile">
-                <button>Car Profile</button>
-            </Link>
-            <Link to="/userprofile">
-                <button>User Profile</button>
-            </Link>
-        </>
+        <div className='navbar'>
+            <img className="car-image" src={car} alt="Car logo"/> 
+
+            <div className='btn-container'>
+                <Link to="/">
+                    <button className="btn-navbar">Home</button>
+                </Link>
+                <Link to="/carprofile">
+                    <button className="btn-navbar">Car Profile</button>
+                </Link>
+                <Link to="/userprofile">
+                    <button className="btn-navbar">User Profile</button>
+                </Link>
+            </div>
+        </div>
     )
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,52 +1,38 @@
-import Popup from "reactjs-popup"
-import 'reactjs-popup/dist/index.css'
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import { Modal } from "../components/Modal";
+import "./Home.css"
+import "../index.css"
 
-export function Home() {
-    return (
-      <>
-        <Popup trigger={<button>+</button>} position="right center"
-          modal nested> {
-            <div>
-              Add Vehicle Information Here
-            </div>
-          }
-          <div>car_name</div>
-          <div>car_type_etc</div>
-        </Popup>
-      </>
-    )
-}
+function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [message, setMessage] = useState("");
 
-{/*
-    import { dummyData } from './data/cars'
-    <main>
-    <h1> Car Maintenance Tracker </h1>
-      <div>
-        {dummyData.slice(0, 3).map((car, index) => (
-          <button key={index} title={car.name}>{car.name}</button>
-        ))}
-      </div>
-   </main>
+  const handleButtonClick = (value: string) => {
+    setModalOpen(false);
+    setMessage(value);
+  };
 
-   <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
-    >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <p>Modal body text goes here.</p>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
+  return (
+    <div className="index-container">
+      {message}
+      <button className="btn-open" onClick={() => setModalOpen(true)}>
+        Add_Car_Button
+      </button>
+      {modalOpen &&
+        createPortal(
+          <Modal 
+            onSubmit={handleButtonClick}
+            onCancel={handleButtonClick}
+            onClose={handleButtonClick}
+          >
+            <h1>Form to add a car</h1>
+            <p>the form will go here</p>
+          </Modal>,
+          document.body
+        )}
     </div>
   );
+}
 
-*/}
+export default Home;
