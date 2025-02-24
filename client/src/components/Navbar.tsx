f5ff2f5e37abec51e1b81044e853e65441356e7f
jackh <jackhyland@mac.com>
2025-02-24 15:45:26 -0500

    Issue #9 Progressed - Added gradient to navbar, title, and updated button/title hover effects. All button fonts updated.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom"
-import "./Navbar.css"
-import car from "../../src/assets/car-image.png"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import car from "../../src/assets/car-image.png";
 
 // Static navigation bar with routing to other pages, cojoined with 'Layout'
 
 export function Navbar() {
     return (
         <div className='navbar'>
-            <img className="car-image" src={car} alt="Car logo"/> 
+            <div className="logo-container">
+                <img className="car-image" src={car} alt="Car logo"/>
+                <span className="nav-title">Maintenance Tracker</span>
+            </div>
 
             <div className='btn-container'>
                 <Link to="/">
@@ -21,5 +24,5 @@ export function Navbar() {
                 </Link>
             </div>
         </div>
-    )
+    );
 }
