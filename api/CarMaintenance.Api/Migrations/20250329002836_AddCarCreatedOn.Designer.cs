f871e2912b46bbd884f3900dda075fd19cc2f6ac
grigdon <gabrielrigdon@gmail.com>
2025-03-28 21:09:41 -0400

    closed #52 - Postgre table updated

--- api/CarMaintenance.Api/Migrations/20250329002836_AddCarCreatedOn.Designer.cs
+++ api/CarMaintenance.Api/Migrations/20250329002836_AddCarCreatedOn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CarMaintenance.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CarMaintenance.Api.Migrations
 {
     [DbContext(typeof(CarDbContext))]
-    partial class CarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250329002836_AddCarCreatedOn")]
+    partial class AddCarCreatedOn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
